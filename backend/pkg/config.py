7a11b619c34c70962291d1703ce60b6a6f13e668
stranavad <stranava.david@gmail.com>
2021-02-21 11:22:40 +0100

    Change password in config.py

--- backend/pkg/config.py
+++ backend/pkg/config.py
@@ -1,6 +1,6 @@
 config = {
     "api_token": "ts9pFkGuXKgcmo43Mmj0^eG%iiR3m",
     "email": "majkoponist@gmail.com",
-    "password": "password123",
+    "password": "Ninuška",
     "db_password": "<Code><Tech> 127521"
 }
