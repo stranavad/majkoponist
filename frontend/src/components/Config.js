216238b7c01224944de2c2b8070a9b856d1a57b4
stranavad <stranava.david@gmail.com>
2021-03-08 16:42:54 +0100

    IP change to new backend domain

--- frontend/src/components/Config.js
+++ frontend/src/components/Config.js
@@ -1,5 +1,5 @@
 const Config = {
     token: 'ts9pFkGuXKgcmo43Mmj0^eG%iiR3m',
-    server_ip: 'http://l39.162.161.156/'
+    server_ip: 'https://backend.apartmanyhana.sk/'
 }
 export default Config;
