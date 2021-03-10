d56534f0c45140b09f2ad49d480e2d8510ba3eac
stranavad <stranava.david@gmail.com>
2021-03-11 00:53:09 +0100

    Change email to Majko

--- backend/pkg/config.py
+++ backend/pkg/config.py
@@ -1,6 +1,6 @@
 config = {
     "api_token": "ts9pFkGuXKgcmo43Mmj0^eG%iiR3m",
-    "email": "majkoponist@gmail.com",
+    "email": "ponist@bauservis.sk",
     "password": "Ninuška",
     "db_password": "<Code><Tech> 127521",
     "mail_to_send": "stranava.david@gmail.com"
