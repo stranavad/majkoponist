f1ebfa77e651bfc5387568d9dec1d80129e41ff0
David Stranava <stranava.david@gmail.com>
2021-09-18 16:18:57 +0200

    Gitignore remove

--- backend/pkg/config.py
+++
@@ -1,9 +0,0 @@
-config = {
-    "api_token": "ts9pFkGuXKgcmo43Mmj0^eG%iiR3m",
-    "email": "ponist@bauservis.sk",
-    "password": "Ninuška",
-    "db_password": "<Code><Tech> 127521",
-    "mail_to_send": "stranava.david@gmail.com",
-    "db_host": "139.162.161.156",
-    "mail_pasword": "8JxYXY^mDVfrN1"
-}
